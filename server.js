8c3d713bb05864b353ced8ea89c1956b4f03cec6
Luis Alfaro <luis.alfaro@omni.cr>
2021-05-03 16:43:27 -0600

    testing2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require ('http');
 function onRequest(request, response){
 	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.write('Hello World Luis');
+	response.write('Hello World Luis Ahora esto es nuevo');
 	response.end();
 
 }
